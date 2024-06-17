34ace0ac584c88abdf9ad049e4ba0a2ba1acf728
Moritz Lucas <moritz.lucas@uni-osnabrueck.de>
2024-06-17 17:15:36 +0200

    Finished the creating masks and added the statistic tool for selecting validation and paper

--- tools/create_masks.py
+++ tools/create_masks.py
@@ -1,35 +1,75 @@
 import geopandas as gpd
 import rasterio
 from rasterio.features import rasterize
-from shapely.geometry import mapping
 import os
+import argparse
 
-from tof.utils import (
-    get_file_list,
-    argparse_TOF,
-)
+
+def argparse_TOF():
+    """Parse the command line arguments for the TOF script"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--state",
+        type=str,
+        help="Set it to the foldernames of your sites as string",
+    )
+    parser.add_argument(
+        "--eroded",
+        type=bool,
+        default=False,
+        help="Set it to True if you want to erode the mask boundaries",
+    )
+    parser.add_argument(
+        "--epsg",
+        type=str,
+        default="EPSG:25832",
+        help="Set the EPSG code of your data",
+    )
+    return parser.parse_args()
+
+
+def get_file_list(directory, extension=".tif", type=None):
+    """Get a list of files with a given extension in a directory and optionally rename them with a prefix"""
+    files = os.listdir(directory)
+    file_list = []
+    for file in files:
+        if type:
+            if file.endswith(".tif"):
+                filename, _ = os.path.splitext(file)
+                new_filename = f"{type}_{filename[4:]}{extension}"
+                new_directory = directory.replace("TOP", type)
+                new_file_path = os.path.join(new_directory, new_filename)
+                # Create the directory if it doesn't exist
+                os.makedirs(new_directory, exist_ok=True)
+                file_list.append(new_file_path)
+        else:
+            if file.endswith(extension):
+                file_list.append(f"{directory}/{file}")
+
+    return sorted(file_list)
+
 
 if __name__ == "__main__":
-
-    # ignore warnings
 
     args = argparse_TOF()
     state = args.state
     epsg = args.epsg
+    eroded = args.eroded
 
-    list_path_RGBI = get_file_list(f"Sites/{state}/TOP")
-    path_Shape = f"Sites/{state}/SHP_noALKIS/{state}_result_merged_reclassified.shp"
+    list_path_RGBI = get_file_list(f"data/sites/{state}/TOP")
+    path_Shape = f"data/sites/{state}/SHP/{state}_TOF.shp"
 
     gdf = gpd.read_file(path_Shape)
+    gdf["classvalue"] = gdf["classvalue"].astype(int)
 
     # Check if mask folder exists
-    if not os.path.exists(f"Sites/{state}/Masks"):
-        os.makedirs(f"Sites/{state}/Masks")
+    if not os.path.exists(f"data/sites/{state}/Masks"):
+        os.makedirs(f"data/sites/{state}/Masks")
 
     for i, path_RGBI in enumerate(list_path_RGBI):
 
         name = path_RGBI.split("/")[-1].replace("TOP", "mask")
-        mask_path = f"Sites/{state}/Masks/{name}"
+        mask_path = f"data/sites/{state}/Masks/{name}"
 
         with rasterio.open(path_RGBI[:-4] + ".tif") as ref_raster:
             with rasterio.open(
@@ -55,6 +95,10 @@ if __name__ == "__main__":
                     dtype=rasterio.uint16,
                     default_value=1,
                 )
+
+                if eroded:
+                    # Implement erosion here
+                    pass
 
                 mask_raster.write(burned, 1)
 
--- tools/data_statistics.py
+++ tools/data_statistics.py
@@ -1,35 +1,77 @@
-import glob
 import os
 import numpy as np
-import cv2
 from PIL import Image
-import multiprocessing.pool as mpp
-import multiprocessing as mp
-import time
 import argparse
 
+
 def parse_args():
+    """Parse the command line arguments for the TOF script"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mask-dir", default="data/tof/train_masks")
-    parser.add_argument("--output-dir", default="data/tof/statistics")
+    parser.add_argument(
+        "--state",
+        type=str,
+        help="Set it to the foldernames of your sites as string",
+    )
     return parser.parse_args()
 
-def calc_stats():
-    
 
-masks_dir = args.mask_dir
+def get_statistics(mask_path, num_classes):
+    mask = Image.open(mask_path)
+    mask_crop = np.array(mask)
+    bins = np.array(range(num_classes))
+    class_pixel_counts, _ = np.histogram(mask_crop, bins=bins)
+    cf = class_pixel_counts / (mask_crop.shape[0] * mask_crop.shape[1])
 
-mask_paths_raw = glob.glob(os.path.join(masks_dir, "*.tif"))
+    return cf
 
-mask = Image.open(mask_path).convert("RGB")
 
-bins = np.array(range(num_classes + 1))
-class_pixel_counts, _ = np.histogram(mask_crop, bins=bins)
-cf = class_pixel_counts / (mask_crop.shape[0] * mask_crop.shape[1])
+if __name__ == "__main__":
+    args = parse_args()
+    state = args.state
+    masks_dir = f"data/sites/{state}/Masks"
+    mask_paths = [
+        file
+        for file in os.listdir(f"data/sites/{state}/Masks")
+        if file.endswith(".tif")
+    ]
+    num_classes = 6
+    cf_list = []
+    for mask_path in mask_paths:
+        mask_path = os.path.join(masks_dir, mask_path)
+        cf = get_statistics(mask_path, num_classes)
+        cf_list.append(cf)
 
-t0 = time.time()
-mpp.Pool(processes=mp.cpu_count()).map(calc_stats, inp)
-t1 = time.time()
+    # do a average over all the masks
+    cf_list = np.array(cf_list)
+    cf_avg = np.mean(cf_list, axis=0)
+    print("Average Distribution", cf_avg)
+    mask_names = [mask_path.split("/")[-1] for mask_path in mask_paths]
+    cf_avg_with_names = list(zip(mask_names, cf_list))
 
-    split_time = t1 - t0
-    print("images spliting spends: {} s".format(split_time))
+    # Save as a txt file
+
+    # Choose 5 of the masks so they have the same distribution as the average
+    mask_paths = np.array(mask_paths)
+    mask_paths_selected = []
+    cf_avg_select = np.zeros(num_classes - 1)
+    while not np.allclose(cf_avg, cf_avg_select, atol=0.01):
+        cf_list = []
+        mask_paths_selected = []
+        for i in range(5):
+            mask_path = mask_paths[np.random.choice(len(mask_paths))]
+            mask_paths_selected.append(mask_path)
+            mask_path = os.path.join(masks_dir, mask_path)
+            cf = get_statistics(mask_path, num_classes)
+            cf_list.append(cf)
+        cf_list = np.array(cf_list)
+        cf_avg_select = np.mean(cf_list, axis=0)
+
+    print("Selected masks", mask_paths_selected)
+
+    # save average distribution and selected masks
+    if not os.path.exists(f"data/sites/{state}/Stats"):
+        os.makedirs(f"data/sites/{state}/Stats")
+    np.savetxt(f"data/sites/{state}/Stats/average_distribution.txt", cf_avg)
+    np.savetxt(
+        f"data/sites/{state}/Stats/selected_masks.txt", mask_paths_selected, fmt="%s"
+    )
