5ed17e74454c1036a9bb7531a4a8573ded80d7af
Moritz Lucas <moritz.lucas@uni-osnabrueck.de>
2024-06-23 22:30:43 +0200

    added strange image loading with tiffile

--- tools/tof_patch_split.py
+++ tools/tof_patch_split.py
@@ -228,9 +228,18 @@ def tof_format(inp):
         mask_path = mask_path[:-4] + "_noBoundary.tif"
     try:
         img = tifffile.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(img)
     except Exception as e:
         print(f"Error opening image: {e}")
-    mask = Image.open(mask_path).convert("RGB")
+    try:
+        mask = tifffile.imread(mask_path)
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+        mask = Image.fromarray(mask)
+    except Exception as e:
+        print(f"Error opening mask: {e}")
+    # img = Image.open(img_path).convert("RGB")
+    # mask = Image.open(mask_path).convert("RGB")
     if gt:
         mask_ = car_color_replace(mask)
         out_origin_mask_path = os.path.join(
