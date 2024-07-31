e037967f9ff8a8ef0d323f1eb1ba8d6c9e46efd4
Moritz Lucas <moritz.lucas@uni-osnabrueck.de>
2024-07-31 16:17:29 +0200

    just config changes

--- geoseg/datasets/tof_dataset.py
+++ geoseg/datasets/tof_dataset.py
@@ -17,7 +17,7 @@ PALETTE = [[255, 255, 255], [0, 0, 255], [0, 255, 255], [0, 255, 0], [255, 204, 
 
 ORIGIN_IMG_SIZE = (1024, 1024)
 INPUT_IMG_SIZE = (1024, 1024)
-TEST_IMG_SIZE = (10000, 10000)
+TEST_IMG_SIZE = (5000, 5000)
 
 
 def get_training_transform():
