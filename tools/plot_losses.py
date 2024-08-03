5d9b42ec8dc6c52a87fc9353867c644e613c0280
Moritz Lucas <moritz.lucas@uni-osnabrueck.de>
2024-08-03 12:15:05 +0200

    added log plotting

--- tools/plot_losses.py
+++ tools/plot_losses.py
@@ -2,7 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load data from the CSV file
-df = pd.read_csv("lightning_logs/tof/dcswin/metrics.csv")
+# df = pd.read_csv("lightning_logs/tof/dcswin/metrics.csv")
+df = pd.read_csv("lightning_logs/tof/unetformer/metrics.csv")
 
 
 # Define a function to calculate the running mean
@@ -19,9 +20,22 @@ df["val_loss_rm"] = running_mean(df["val_loss"], window_size=15)
 def plot_metrics(df, x, metrics):
     plt.figure(figsize=(15, 10))
 
+    max_val_f1_epoch = df[df["val_F1"] == df["val_F1"].max()][x].values[0]
+
     for metric in metrics:
         data = df[[x, metric]].dropna()
         plt.plot(data[x], data[metric], label=metric)
+
+    # Plot a red vertical line at the epoch with maximum val_F1
+    plt.axvline(
+        x=max_val_f1_epoch,
+        color="red",
+        linestyle="dashed",
+        linewidth=1,
+        label="Model chosen",
+    )
+
+    print("max val_F1 epoch", max_val_f1_epoch)
 
     plt.xlabel(x)
     plt.ylabel("Value")
@@ -39,9 +53,9 @@ metrics_to_plot = [
     "val_OA",
     "val_mIoU",
     # "val_IoU_Forest",
-    # "val_IoU_Tree",
+    "val_IoU_Tree",
     # "val_IoU_Background",
-    # "val_IoU_Linear",
+    "val_IoU_Linear",
     # "val_loss",
     # "val_IoU_Patch",
     "val_F1",
@@ -55,6 +69,10 @@ metrics_to_plot = [
     # "train_mIoU",
     # "train_IoU_Patch",
 ]
+
+# print max val_F1 and corresponding epoch
+print("max val_F1 epoch", df.loc[df["val_F1"].idxmax()]["epoch"])
+print("max val_F1", df["val_F1"].max())
 
 # Convert columns to appropriate types (e.g., numeric)
 df = df.apply(pd.to_numeric, errors="coerce")
