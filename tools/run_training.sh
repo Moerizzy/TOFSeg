7d2695734c774097328512872bcbeba18f59d2e0
Moritz Lucas <moritz.lucas@uni-osnabrueck.de>
2024-08-17 00:58:19 +0200

    chages bash script for training

--- tools/run_training.sh
+++ tools/run_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python GeoSeg/train_supervision.py -c GeoSeg/config/tof/unetformer.py
-python GeoSeg/train_supervision.py -c GeoSeg/config/tof/dcswin.py
+#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/unetformer.py
+#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/dcswin.py
 python GeoSeg/train_supervision.py -c GeoSeg/config/tof/ftunetformer.py
 python GeoSeg/train_supervision.py -c GeoSeg/config/tof/banet.py
 python GeoSeg/train_supervision.py -c GeoSeg/config/tof/abcnet.py
