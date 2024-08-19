9aa4e290a995012f265c2c0b90624cdcae65a925
Moritz Lucas <moritz.lucas@uni-osnabrueck.de>
2024-08-19 12:09:19 +0200

    changed run_training.sh

--- tools/run_training.sh
+++ tools/run_training.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#_
-python GeoSeg/train_supervision.py -c GeoSeg/config/tof/dcswin.py
-python GeoSeg/train_supervision.py -c GeoSeg/config/tof/ftunetformer.py
+#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/unetformer.py
+#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/dcswin.py
+#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/ftunetformer.py
 python GeoSeg/train_supervision.py -c GeoSeg/config/tof/banet.py
-python GeoSeg/train_supervision.py -c GeoSeg/config/tof/abcnet.py
-python GeoSeg/train_supervision.py -c GeoSeg/config/tof/a2fpn.py
+#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/abcnet.py
+#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/a2fpn.py
 python GeoSeg/train_supervision.py -c GeoSeg/config/tof/unetformer_lsk_s.py
 python GeoSeg/train_supervision.py -c GeoSeg/config/tof/unetformer_lsk_t.py
