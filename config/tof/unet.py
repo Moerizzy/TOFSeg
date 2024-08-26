2d0ec38ffa43b28b2d4493e34d19badea99a72fe
Moritz Lucas <moritz.lucas@uni-osnabrueck.de>
2024-08-26 16:25:33 +0200

    added unet

--- config/tof/unet.py
+++ config/tof/unet.py
@@ -27,7 +27,11 @@ save_top_k = 1
 save_last = False
 check_val_every_n_epoch = 1
 pretrained_ckpt_path = None  # the path for the pretrained model weight
-gpus = "auto"  # [1, 2, 3] #  default or gpu ids:[0] or gpu nums: 2, more setting can refer to pytorch_lightning
+gpus = [
+    1,
+    2,
+    3,
+]  #  default or gpu ids:[0] or gpu nums: 2, more setting can refer to pytorch_lightning
 resume_ckpt_path = None  # whether continue training with the checkpoint, default None
 
 #  define the network
