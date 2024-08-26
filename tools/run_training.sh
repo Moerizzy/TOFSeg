bb378aac74ccd121c91a30cbe89bef3096de8605
Moritz Lucas <moritz.lucas@uni-osnabrueck.de>
2024-08-26 20:39:08 +0200

    added tif and tof class config

--- tools/run_training.sh
+++ tools/run_training.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
-#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/unetformer.py
-#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/dcswin.py
-python GeoSeg/train_supervision.py -c GeoSeg/config/tof/dcswin_tiny.py
-python GeoSeg/train_supervision.py -c GeoSeg/config/tof/dcswin_base.py
-#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/ftunetformer.py
-#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/banet.py
-#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/abcnet.py
-#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/a2fpn.py
-#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/unetformer_lsk_s.py
-#python GeoSeg/train_supervision.py -c GeoSeg/config/tof/unetformer_lsk_t.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tof/unet.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/unet.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/unetformer.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/dcswin.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/dcswin_tiny.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/dcswin_base.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/ftunetformer.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/banet.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/abcnet.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/a2fpn.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/unetformer_lsk_s.py
+python GeoSeg/train_supervision.py -c GeoSeg/config/tif_tof/unetformer_lsk_t.py
