ea97cd94f2fea99a92303fe1176c081d8b3e56c8
Moritz Lucas <moritz.lucas@uni-osnabrueck.de>
2024-08-28 09:37:41 +0200

    added run_testing_tif_tof.sh

---
+++ tools/run_testing_tif_tof.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+python GeoSeg/tof_test.py -c -c GeoSeg/config/tof/unet.py -o fig_results/tof/unet
+python GeoSeg/tools/stitch_together.py "fig_results/tof/unet"
+python GeoSeg/tof_test.py -c -c GeoSeg/config/tif_tof/unet.py -o fig_results/tif_tof/unet
+python GeoSeg/tof_test.py -c GeoSeg/config/tif_tof/unetformer.py -o fig_results/tif_tof/unetformer
+python GeoSeg/tools/stitch_together.py "fig_results/tif_tof/unetformer"
+python GeoSeg/tof_test.py -c GeoSeg/config/tif_tof/dcswin_small.py -o fig_results/tif_tof/dcswin_small
+python GeoSeg/tools/stitch_together.py "fig_results/tif_tof/dcswin_small"
+python GeoSeg/tof_test.py -c GeoSeg/config/tif_tof/dcswin_tiny.py -o fig_results/tif_tof/dcswin_tiny
+python GeoSeg/tools/stitch_together.py "fig_results/tif_tof/dcswin_tiny"
+python GeoSeg/tof_test.py -c GeoSeg/config/tif_tof/dcswin_base.py -o fig_results/tif_tof/dcswin_base
+python GeoSeg/tools/stitch_together.py "fig_results/tif_tof/dcswin_base"
+python GeoSeg/tof_test.py -c GeoSeg/config/tif_tof/ftunetformer.py -o fig_results/tif_tof/ftunetformer
+python GeoSeg/tools/stitch_together.py "fig_results/tif_tof/ftunetformer"
+python GeoSeg/tof_test.py -c GeoSeg/config/tif_tof/banet.py -o fig_results/tif_tof/banet
+python GeoSeg/tools/stitch_together.py "fig_results/tif_tof/banet"
+python GeoSeg/tof_test.py -c GeoSeg/config/tif_tof/abcnet.py -o fig_results/tif_tof/abcnet
+python GeoSeg/tools/stitch_together.py "fig_results/tif_tof/abcnet"
+python GeoSeg/tof_test.py -c GeoSeg/config/tif_tof/a2fpn.py -o fig_results/tif_tof/a2fpn
+python GeoSeg/tools/stitch_together.py "fig_results/tif_tof/a2fpn"
+python GeoSeg/tof_test.py -c GeoSeg/config/tif_tof/unetformer_lsk_s.py -o fig_results/tif_tof/unetformer_lsk_s
+python GeoSeg/tools/stitch_together.py "fig_results/tif_tof/unetformer_lsk_s"
+python GeoSeg/tof_test.py -c GeoSeg/config/tif_tof/unetformer_lsk_t.py -o fig_results/tif_tof/unetformer_lsk_t
+python GeoSeg/tools/stitch_together.py "fig_results/tif_tof/unetformer_lsk_t"
