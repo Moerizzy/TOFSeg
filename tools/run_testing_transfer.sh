a54345e5b36edcc95037d4eca904cbdf82390641
Moritz Lucas <moritz.lucas@uni-osnabrueck.de>
2024-08-29 13:59:06 +0200

    added run_testing_transfer

--- tools/run_testing_transfer.sh
+++ tools/run_testing_transfer.sh
@@ -6,8 +6,8 @@ python GeoSeg/tools/stitch_together.py "fig_results/tof/ftunetformer_com1"
 python GeoSeg/tof_test.py -c GeoSeg/config/tof_transfer/ftunetformer_com2.py -o fig_results/tof_transfer/ftunetformer_com2
 python GeoSeg/tools/stitch_together.py "fig_results/tof/ftunetformer_com2"
 
-python GeoSeg/tof_test.py -c GeoSeg/config/tof_transfer/ftunetformer_com3.py -o fig_results/tof_transfer/ftunetformer_com3
-python GeoSeg/tools/stitch_together.py "fig_results/tof/ftunetformer_com3"
+# python GeoSeg/tof_test.py -c GeoSeg/config/tof_transfer/ftunetformer_com3.py -o fig_results/tof_transfer/ftunetformer_com3
+# python GeoSeg/tools/stitch_together.py "fig_results/tof/ftunetformer_com3"
 
-python GeoSeg/tof_test.py -c GeoSeg/config/tof_transfer/ftunetformer_com4.py -o fig_results/tof_transfer/ftunetformer_com4
-python GeoSeg/tools/stitch_together.py "fig_results/tof/ftunetformer_com4"
+# python GeoSeg/tof_test.py -c GeoSeg/config/tof_transfer/ftunetformer_com4.py -o fig_results/tof_transfer/ftunetformer_com4
+# python GeoSeg/tools/stitch_together.py "fig_results/tof/ftunetformer_com4"
